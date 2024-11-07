8e9675ef066a9b8dfcbf14e71909b980b19352c0
Simon Sieber <simon-sieber@gmx.de>
2024-11-07 08:46:34 +0100

    ''

--- game/game.js
+++ game/game.js
@@ -1,7 +1,9 @@
 "use strict"
 
-const RandomWalkCircleElement = require('./randomwalkcircleelement')
-const ElementList = require('./elementlist')
+const player = require('./player')
+let playerdraw = new player()
+
+
 
 // DAS IST EIN NEUER TEST KOMMENTAR
 
@@ -9,47 +11,43 @@ const ElementList = require('./elementlist')
 
 module.exports = class Game {
 
-    constructor() {
-        this.raf                       // request animation frame handle
-        this.elementList = null
+    constructor() {}
+
+    drawMenu() {
+
     }
 
-    //----------------------
+
 
     start() {
-        this.elementList = new ElementList()
-        for (let i = 0; i < 60; i++) {
-            this.elementList.add(new RandomWalkCircleElement(i * 10, 150))
-        }
-
-        this.timeOfLastFrame = Date.now()
-        this.raf = window.requestAnimationFrame(this.tick.bind(this))
-    }
-
-    //----------------------
-
-    stop() {
-        window.cancelAnimationFrame(this.raf)
-        this.elementList = null
-    }
-
-    //----------------------
-
-    tick() {
         let mycanvas = window.document.getElementById("mycanvas")
 
         let ctx = mycanvas.getContext('2d')
 
-        //--- clear screen
-        ctx.fillStyle = 'rgba(235, 250, 255, 0.05)'        // alpha < 1 löscht den Bildschrim nur teilweise -> bewegte Gegenstände erzeugen Spuren
-        ctx.fillRect(0, 0, mycanvas.clientWidth, mycanvas.clientHeight)
+        playerdraw.drawPlayer(ctx, 1280 / 2, 720 / 2)
+    }
 
-        //--- draw elements
-        this.elementList.draw(ctx)
 
-        //--- execute element actions
-        this.elementList.action()
 
-        this.raf = window.requestAnimationFrame(this.tick.bind(this))
+    update() {
+
+    }
+
+
+
+    end() {
+
+    }
+
+
+
+    pause() {
+
+    }
+
+
+
+    updateFrames() {
+
     }
 }
---
+++ game/platform.js
@@ -0,0 +1,16 @@
+"use strict"
+
+module.exports = class Element {
+
+    draw(ctx) {
+        
+    }
+
+
+
+    move() {
+        
+    }
+
+}
+
