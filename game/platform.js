e48fef9f90790cd6a38f8df4bd89fd65e3f7768c
ID-9 <imke.deutschlaender@student.uni-halle.de>
2024-11-13 16:30:06 +0100

    Update platform.js

--- game/platform.js
+++ game/platform.js
@@ -1,14 +1,31 @@
 "use strict"
+const {
+    ctx
+} = require('./platform.js')
 
-module.exports = class Element {
+module.exports = class Platform {
 
-    draw(ctx) {
-        
+    constructor({ position}) {
+        this.position = position
+        this.width = 30
+        this.height = 30
+      }
+    
+      
+    update () {
+   // this.draw()
+   ctx.fillStyle = 'rgba(255, 0, 0, 0,5)'
+   ctx.fillRect(this.position.x, this.position.y, this.width, this.height)
+   }
+
+
+   draw(ctx) {
+    ctx.fillStyle = 'rgba(255, 0, 0, 0,5)'
+   ctx.fillRect(this.position.x, this.position.y, this.width, this.height)
     }
 
 
-
-    move() {
+     move() {
         
     }
 
