a832eb37a51c25b032390578833e60f595415c12
ID-9 <imke.deutschlaender@student.uni-halle.de>
2024-11-13 22:52:22 +0100

    Update game.js
    platform udpate in tick()

--- game/game.js
+++ game/game.js
@@ -1,6 +1,7 @@
 "use strict"
 
 const Player = require('./player.js')
+const Platform = require("./platform")
 const {
   canvas,
   ctx,
@@ -8,9 +9,8 @@ const {
   startedPressingJump,
   stoppedPressingJump,
   gameHelpers,
-  collisionBlocks,
-  platformCollisionBlocks
-   
+  floor,
+  platform   
 } = require('./constants.js')
 
 module.exports = class Game {
@@ -124,8 +124,6 @@ module.exports = class Game {
     // drawing elements
     this.player.update()
 
-    this.platform.update()
-
     //ctx.fillRect(300, 510, 120, 30)
     // handling player moving on x-axis
     if (this.player.velocity.x > 0) this.player.velocity.x -= 0.2
@@ -136,6 +134,53 @@ module.exports = class Game {
   
      // drawing floor and platforms
    
+ //teilt floorArray ist gleichlange Arrays der Canvasbreite   
+ const floorCollisions2D = []
+ for (let i = 0; i < floor.length; i += 32) {
+   floorCollisions2D.push(floor.slice(i, i + 32))
+ }
+ 
+ //teilt die gleichlangen Arrays der Canvasbreite in floor-platforms
+ const collisionBlocks = []
+ floorCollisions2D.forEach((row, y) => {
+   row.forEach((symbol, x) => {
+     if (symbol === 1) {
+        collisionBlocks.push(
+         new Platform({
+           position: {
+             x: x * 30,
+             y: y * 30,
+           },
+         })
+       )
+     }
+   })
+ })
+ 
+ //teilt platform-Array ist gleichlange Arrays der Canvasbreite   
+ const platformCollisions2D = []
+ for (let i = 0; i < platform.length; i += 32) {
+   platformCollisions2D.push(platform.slice(i, i + 32))
+ }
+ 
+ //teilt die gleichlangen Arrays der Canvasbreite in platforms
+ const platformCollisionBlocks = []
+ platformCollisions2D.forEach((row, y) => {
+   row.forEach((symbol, x) => {
+     if (symbol === 1) {
+       platformCollisionBlocks.push(
+         new Platform({
+           position: {
+             x: x * 30,
+             y: y * 30,
+           },
+         })
+       )
+     }
+   })
+ })
+
+
     // Zeichne alle floorblocks
     collisionBlocks.forEach(block => {
         block.update()
@@ -152,4 +197,3 @@ module.exports = class Game {
     this.raf = window.requestAnimationFrame(this.tick.bind(this))
   }
 }
-
