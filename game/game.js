3e4b7592ba94b2e664241e938e3b9da2849e0f38
pauljer <pauljergus@gmail.com>
2024-11-14 20:38:34 +0100

    merging...

--- game/game.js
+++ game/game.js
@@ -40,64 +40,7 @@ module.exports = class Game {
       width: 50,
     })
 
-// drawing floor and platforms
-  //teilt floorArray ist gleichlange Arrays der Canvasbreite   
-  const floorCollisions2D = []
-for (let i = 0; i < floor.length; i += 32) {
-  floorCollisions2D.push(floor.slice(i, i + 32))
-}
-
-//teilt die gleichlangen Arrays der Canvasbreite in floor-platforms
-const collisionBlocks = []
-floorCollisions2D.forEach((row, y) => {
-  row.forEach((symbol, x) => {
-    if (symbol === 1) {
-      collisionBlocks.push(
-        new Platform({
-          position: {
-            x: x * 30,
-            y: y * 30,
-          },
-        })
-      )
-    }
-  })
-})
-
-//teilt platform-Array ist gleichlange Arrays der Canvasbreite   
-const platformCollisions2D = []
-for (let i = 0; i < platform.length; i += 32) {
-  platformCollisions2D.push(platform.slice(i, i + 32))
-}
-
-//teilt die gleichlangen Arrays der Canvasbreite in platforms
-const platformCollisionBlocks = []
-platformCollisions2D.forEach((row, y) => {
-  row.forEach((symbol, x) => {
-    if (symbol === 1) {
-      platformCollisionBlocks.push(
-        new Platform({
-          position: {
-            x: x * 30,
-            y: y * 30,
-          },
-        })
-      )
-    }
-  })
-})
-  
-  // Zeichne alle floorblocks
-  collisionBlocks.forEach(block => {
-      block.draw(ctx); 
-  });
-  
-  // Zeichne alle platformblocks
-    platformCollisionBlocks.forEach(block => {
-      block.draw(ctx); 
-  });
-    
-    // only debugging
+            // only debugging
     const debugBtn = document.getElementById('show-state')
     debugBtn.addEventListener('click', () => {
       console.log(gameState)
@@ -129,3 +72,4 @@ platformCollisions2D.forEach((row, y) => {
     this.raf = window.requestAnimationFrame(this.tick.bind(this))
   }
 }
+
